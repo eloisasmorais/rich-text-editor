8c244e25ec680607a3931d90762fda4d787bc389
Eloisa Morais <eloisa.smorais@gmail.com>
2020-02-14 21:58:47 -0300

    Removes unused code

--- js/script.js
+++ js/script.js
@@ -8,13 +8,3 @@ function toggleCommand($command) {
   document.execCommand($command, false, null);
   textarea.focus();
 }
-
-const bold = document.getElementById('bold');
-const italic = document.getElementById('italic');
-const underline = document.getElementById('underline');
-const list = document.getElementById('list');
-
-bold.addEventListener('onclick', toggleCommand('bold'));
-italic.addEventListener('onclick', toggleCommand('italic'));
-underline.addEventListener('onclick', toggleCommand('underline'));
-list.addEventListener('onclick', toggleCommand('list'));
