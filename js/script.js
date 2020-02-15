d679ce65cd8ce03b5663e9c2d5bb62704aa531aa
Eloisa Morais <eloisa.smorais@gmail.com>
2020-02-14 21:56:57 -0300

    Add functions to enable editing, add new fonts and final styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+var textarea = document.getElementById('text-area');
 window.onload = function() {
-  var textarea = document.getElementById('text-area');
   textarea.designMode = 'on';
   textarea.focus();
-  console.log(textarea);
 };
+
+function toggleCommand($command) {
+  document.execCommand($command, false, null);
+  textarea.focus();
+}
+
+const bold = document.getElementById('bold');
+const italic = document.getElementById('italic');
+const underline = document.getElementById('underline');
+const list = document.getElementById('list');
+
+bold.addEventListener('onclick', toggleCommand('bold'));
+italic.addEventListener('onclick', toggleCommand('italic'));
+underline.addEventListener('onclick', toggleCommand('underline'));
+list.addEventListener('onclick', toggleCommand('list'));
